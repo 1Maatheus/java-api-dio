5edb3cf47a55430328ae3399dc60ba343a443d0a
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-10-04 12:59:32 -0300

    initial commit

---
+++ src/main/java/me/dio/projetosantander/ProjetoSantanderApplication.java
@@ -0,0 +1,13 @@
+package me.dio.projetosantander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSantanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/projetosantander/ProjetoSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.projetosantander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
